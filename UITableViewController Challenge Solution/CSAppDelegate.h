89a004002a7d87a73eed7cb1654fba13fa6a09c7
zdrilx <zzz@zzz.ch>
2014-01-25 16:44:02 +0100

    Initial Commit

---
+++ UITableViewController Challenge Solution/CSAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CSAppDelegate.h
+//  UITableViewController Challenge Solution
+//
+//  Created by Tristan Zand on 25.01.14.
+//  Copyright (c) 2014 Tristan Zand. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CSAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ UITableViewController Challenge Solution/CSViewController.h
@@ -0,0 +1,13 @@
+//
+//  CSViewController.h
+//  UITableViewController Challenge Solution
+//
+//  Created by Tristan Zand on 25.01.14.
+//  Copyright (c) 2014 Tristan Zand. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CSViewController : UIViewController
+
+@end
